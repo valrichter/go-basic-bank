823dd3756babd341b047956b884316b51ee890e5
valrichter <valenrichter11@gmail.com>
2023-09-03 19:44:35 -0300

    add CRUD accounts with sqlc

--- db/query/account.sql
+++ db/query/account.sql
@@ -7,3 +7,23 @@ INSERT INTO account (
   $1, $2, $3
 )
 RETURNING *;
+
+-- name: GetAccount :one
+SELECT * FROM account 
+WHERE id = $1 LIMIT 1;
+
+-- name: ListAccount :many
+SELECT * FROM account
+ORDER BY id
+LIMIT $1
+OFFSET $2;
+
+-- name: UpdateAccount :exec
+UPDATE account
+  set balance = $2
+WHERE id = $1
+RETURNING *;
+
+-- name: DeleteAccount :exec
+DELETE FROM account
+WHERE id = $1;
