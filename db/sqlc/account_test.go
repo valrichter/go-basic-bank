cdb7b3c3386179390ecbaefada78cc2e70167442
valrichter <valenrichter11@gmail.com>
2023-09-07 18:50:33 -0300

    add test Gut & Update account & add make test

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -3,15 +3,17 @@ package db
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
+	"github.com/valrichter/basic-system-bank/util"
 )
 
-func TestCreateAccount(t *testing.T){
-	arg := CreateAccountParams {
-		Owner: "valentin",
-		Balance: 1000.00,
-		Currency: "ARS",		
+func createRandomAccount(t *testing.T) Account {
+		arg := CreateAccountParams {
+		Owner: util.RandomOwner(),
+		Balance: util.RandomMoney(),
+		Currency: util.RandomCurrency(),		
 	}
 
 	account, err := testQueries.CreateAccount(context.Background(), arg)
@@ -22,4 +24,40 @@ func TestCreateAccount(t *testing.T){
 	require.Equal(t, arg.Currency, account.Currency)
 	require.NotZero(t, account.ID)
 	require.NotZero(t, account.CreatedAt)
+	
+	return account
+}
+
+func TestCreateAccount(t *testing.T){
+	createRandomAccount(t)
+}
+
+func TestGetAccount(t *testing.T){
+	account1 := createRandomAccount(t)
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+	require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, account1.Owner, account2.Owner)
+	require.Equal(t, account1.Balance, account2.Balance)
+	require.Equal(t, account1.Currency, account2.Currency)
+	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
+}
+
+func TestUpdateAccount(t *testing.T){
+	account1 := createRandomAccount(t)
+
+	arg := UpdateAccountParams{
+		ID: account1.ID,
+		Balance: util.RandomMoney(),
+	}
+	
+	account2, err := testQueries.UpdateAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+	require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, account1.Owner, account2.Owner)
+	require.Equal(t, arg.Balance, account2.Balance)
+	require.Equal(t, account1.Currency, account2.Currency)
+	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
 }
