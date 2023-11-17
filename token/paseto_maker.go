01a01e35e2ac75e6be49f0119e96181e7d141032
valrichter <valenrichter11@gmail.com>
2023-11-17 00:17:43 -0300

    add API Sessions: ahora la seciones son de 1 Dia

--- token/paseto_maker.go
+++ token/paseto_maker.go
@@ -28,13 +28,13 @@ func NewPasetoMaker(symmetricKey string) (Maker, error) {
 }
 
 // CreateToken implements Maker.
-func (maker *PasetoMaker) CreateToken(username string, duration time.Duration) (string, error) {
+func (maker *PasetoMaker) CreateToken(username string, duration time.Duration) (string, *Payload, error) {
 	payload, err := NewPayload(username, duration)
 	if err != nil {
-		return "", err
+		return "", payload, err
 	}
-
-	return maker.paseto.Encrypt(maker.symmetricKey, payload, nil)
+	token, err := maker.paseto.Encrypt(maker.symmetricKey, payload, nil)
+	return token, payload, err
 }
 
 // VerifyToken implements Maker.
