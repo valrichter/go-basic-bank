65d71d1eb8b51ad35578d2cdc20cceaf09bf06bd
valrichter <valenrichter11@gmail.com>
2023-11-17 15:55:19 -0300

    fix docker-compose: ahora funciona el deploy local

--- start_db.sh
+++ start_db.sh
@@ -4,7 +4,9 @@ set -e
 
 echo "run db migration"
 source /app/app.env
+DB_SOURCE=postgresql://root:secret@postgres:5432/bank?sslmode=disable
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
+
 
 echo "start the app"
 
