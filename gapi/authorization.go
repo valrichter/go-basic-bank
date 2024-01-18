a6a4043c172cb100bc3f70e20e96d31051358e9b
valrichter <valenrichter11@gmail.com>
2024-01-18 12:49:24 -0300

    fix role

--- gapi/authorization.go
+++ gapi/authorization.go
@@ -14,7 +14,7 @@ const (
 	authorizationBearer = "bearer"
 )
 
-func (server *Server) authorizeUser(ctx context.Context) (*token.Payload, error) {
+func (server *Server) authorizeUser(ctx context.Context, accessibleRoles[]string) (*token.Payload, error) {
 	md, ok := metadata.FromIncomingContext(ctx)
 	if !ok {
 		return nil, fmt.Errorf("missing metadata")
@@ -42,5 +42,18 @@ func (server *Server) authorizeUser(ctx context.Context) (*token.Payload, error)
 		return nil, fmt.Errorf("invalid access token: %s", err)
 	}
 
+	if !hasPermission(payload.Role, accessibleRoles) {
+		return nil, fmt.Errorf("unauthorized")
+	}
+
 	return payload, nil
+}
+
+func hasPermission(role string, roles []string) bool {
+	for _, r := range roles {
+		if role == r {
+			return true
+		}
+	}
+	return false
 }
